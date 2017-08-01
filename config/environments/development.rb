ace317f9c348f823c0fea5c8ba17635c5a26558d
Rupali <rupalirordev11@gmail.com>
2017-08-01 20:12:10 +0530

    Fixed header footer and ui changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,8 @@ Rails.application.configure do
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
+  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+
   config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
